389c575ac38669c25e31180bb876a1825a8c97a3
mhmad.kavosi <mhmad.kavosi@gmai.com>
2022-08-28 08:18:46 +0430

    get and create user controller

--- src/api/controllers/user.ts
+++ src/api/controllers/user.ts
@@ -1,9 +1,27 @@
+import UserService from '@/services/user';
 import { Request, Response } from 'express';
 
 
+const userService = new UserService();
+
 export const getUser = async (req: Request, res: Response) => {
     try {
-        return res.json({ user: 'user' }).status(200);
+        const user = await userService.findUsers()
+        return res.json({ user: user }).status(200);
+    } catch (error) {
+        res.json({
+            error
+        }).status(400)
+    }
+}
+
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        const user = await userService.createUser(req.body);
+        return res.status(201).json({
+            message: true,
+            data: user
+        })
     } catch (error) {
         res.json({
             error
