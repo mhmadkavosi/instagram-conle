5e751b0a5c80a8daf01074bb1c1da2d51486b755
mhmad.kavosi <mhmad.kavosi@gmai.com>
2022-08-28 11:20:29 +0430

    posts controllers

---
+++ src/api/controllers/post.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from 'express';
+import { PostService } from './../../services/post';
+
+const postService = new PostService();
+
+export const getPosts = async (req: Request, res: Response) => {
+    try {
+        const data = await postService.getPosts();
+        res.json({
+            message: true,
+            data
+        }).status(200)
+    } catch (error) {
+        res.json({
+            error
+        }).status(500)
+    }
+}
+
+export const getPost = async (req: Request, res: Response) => {
+    try {
+        const data = await postService.getPost({ _id: req.params.id });
+        res.json({
+            message: true,
+            data
+        })
+    } catch (error) {
+        res.json({
+            error
+        }).status(500)
+    }
+}
+
+
+export const createPost = async (req: Request, res: Response) => {
+    try {
+        const data = postService.createPost(req.body)
+
+        res.json({
+            message: true,
+            data
+        }).status(201)
+    } catch (error) {
+        res.json({
+            error
+        }).status(500)
+    }
+}
