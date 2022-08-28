2c303b2755c0a359d44547a468eb3b91405e5ddb
mhmad.kavosi <mhmad.kavosi@gmai.com>
2022-08-28 09:20:19 +0430

    add hashtag route endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import user from './routes/user'
-
+import hashtag from "./routes/hashtags";
 // guaranteed to get dependencies
 export default () => {
     const app = Router();
     user(app);
-
+    hashtag(app);
     return app
 }
