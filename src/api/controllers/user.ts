8b16bd38c1b87a3b8ffc456e106ca852c130a1c4
mhmad.kavosi <mhmad.kavosi@gmai.com>
2022-08-29 12:42:10 +0430

    follow and unfollow

--- src/api/controllers/user.ts
+++ src/api/controllers/user.ts
@@ -1,7 +1,6 @@
 import UserService from '@/services/user';
 import { Request, Response } from 'express';
 
-
 const userService = new UserService();
 
 export const getUsers = async (req: Request, res: Response) => {
@@ -15,10 +14,15 @@ export const getUsers = async (req: Request, res: Response) => {
     }
 }
 
+//  TODO : get user info 
 export const getUser = async (req: Request, res: Response) => {
     try {
-        const user = await userService.findUser({ _id: req.params.id })
-        return res.json({ user }).status(200)
+        const user = await userService.findUser({ _id: req.user._id })
+        return res.json({
+            user,
+            followersCount: user.followers.length,
+            followingCount: user.following.length
+        }).status(200)
     } catch (error) {
         res.json({
             error
@@ -37,5 +41,46 @@ export const createUser = async (req: Request, res: Response) => {
         res.json({
             error
         }).status(400)
+    }
+}
+
+export const followUser = async (req: Request, res: Response) => {
+    try {
+        // @ts-ignore
+        const userId = req.user._id;
+        const userWantToFollow = req.body.userId;
+        const user = await userService.findUser({ _id: userId });
+
+
+        const updateFollowing = await userService.updateUser({ _id: user._id }, { $set: { followers: userWantToFollow } })
+        const updateFollower = await userService.updateUser({ _id: userWantToFollow }, { $set: { following: userId } })
+
+        res.json({
+            message: true,
+        }).status(200)
+    } catch (error) {
+        res.json({
+            error
+        }).status(500);
+    }
+}
+
+export const unfollowUser = async (req: Request, res: Response) => {
+    try {
+        // @ts-ignore
+        const userId = req.user._id;
+        const userWantToFollow = req.body.userId;
+        const user = await userService.findUser({ _id: userId });
+
+        const updateFollowing = await userService.updateUser({ _id: user._id }, { $unset: { followers: userWantToFollow } })
+        const updateFollower = await userService.updateUser({ _id: userWantToFollow }, { $unset: { following: userId } })
+
+        res.json({
+            message: true,
+        }).status(200)
+    } catch (error) {
+        res.json({
+            error
+        }).status(500);
     }
 }
