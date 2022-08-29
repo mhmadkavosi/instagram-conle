cb8551f510d2d234750f603e2e872200671b86ca
mhmad.kavosi <mhmad.kavosi@gmai.com>
2022-08-29 12:42:57 +0430

    follow and unfollow routes

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -1,11 +1,13 @@
-import { createUser, getUser } from './../controllers/user';
+import { followUser, getUser, unfollowUser } from './../controllers/user';
 import { Router } from 'express';
+import requiresUser from '../middlewares/requiresUser';
 
 const route = Router();
 
 export default (app: Router) => {
     app.use('/users', route);
 
-    // route.get('/me', getUser);
-    // route.post('/', createUser)
+    route.get('/me', requiresUser, getUser);
+    route.post('/follow', requiresUser, followUser)
+    route.post('/unfollow', requiresUser, unfollowUser)
 };
