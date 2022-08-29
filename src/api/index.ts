7356c01303a699945c293bc866ce14accd987ca0
mhmad.kavosi <mhmad.kavosi@gmai.com>
2022-08-29 09:31:23 +0430

    init auth routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import user from './routes/user'
 import hashtag from "./routes/hashtags";
 import post from "./routes/post";
+import auth from './routes/auth';
 // guaranteed to get dependencies
 export default () => {
     const app = Router();
     user(app);
     hashtag(app);
     post(app)
+    auth(app)
     return app
 }
