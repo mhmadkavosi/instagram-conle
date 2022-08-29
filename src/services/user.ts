9174d3a21ed2df79ca74a110c3e2f08ceb960a68
mhmad.kavosi <mhmad.kavosi@gmai.com>
2022-08-29 09:13:18 +0430

    validate password user

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,26 +1,45 @@
-import { DocumentDefinition } from "mongoose";
+import { omit } from "lodash";
+import { DocumentDefinition, FilterQuery } from "mongoose";
 
-import User, { UserDocument } from '../models/user'
-
-
+import User, { UserDocument } from "../models/user";
 
 export default class UserService {
-
     public async findUsers(): Promise<any> {
-        const user = await User.find()
+        const user = await User.find();
         return user;
     }
 
-    /**
-     * createUset
-     */
+    public async findUser(query: FilterQuery<UserDocument>) {
+        return await User.findOne(query).lean();
+    }
+
     public async createUser(input: DocumentDefinition<UserDocument>) {
         try {
             return await User.create(input);
-
         } catch (error) {
-            throw new Error(error)
+            throw new Error(error);
+        }
+    }
+
+    public async validatePassword({
+        email,
+        password,
+    }: {
+        email: UserDocument["email"];
+        password: string;
+    }) {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return false;
         }
 
+        const isValid = await user.comparePassword(password);
+
+        if (!isValid) {
+            return false;
+        }
+
+        return omit(user.toJSON(), "password");
     }
 }
